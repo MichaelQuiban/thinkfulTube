4760803180c4c6862c87a449c99949c5e882cd73
Michael Q <MichaelQuiban@outlook.com>
2016-08-16 22:29:05 -0700

    Added CSS, made easier class names, added Youtube data API.
    Added functionality to the search button with the Youtube API.
    Set it to the far right of the page.
    Changed class names.
    
    Not Mobile Friendly.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("hello world!");
+$(function () {
+	$("form").on("submit", function(e){
+		e.preventDefault();
+		var request = gapi.client.youtube.search.list({
+		part: "snippet",
+		type: "video",
+		q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+		maxResults: 3,
+		order: "viewCount",
+		publishedAfter: "2015-01-01T00:00:00Z"
+		});
+		request.execute(function(response)) {
+			console.log(reponse);
+		}
+	});
+});
+
+function init() {
+	gapi.client.setApiKey("AIzaSyC3V8D2JAlnTsGCKgMi_VHUzgSTjfvWWxQ");
+	gapi.client.load("youtube", "v3", function() {
+
+	});	
+
+
+}
